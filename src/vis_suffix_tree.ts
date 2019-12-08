9cb30cfd2789ada556c247ac833c4871051e004b
kg86 <kg86dev@gmail.com>
2019-12-08 11:56:15 +0900

    add related links

--- src/vis_suffix_tree.ts
+++ src/vis_suffix_tree.ts
@@ -6,10 +6,7 @@ import * as visjs_default_options from './visjs_default_options'
 const options = visjs_default_options.options
 const container = document.getElementById('network') as HTMLElement
 const network = new vis.Network(container, {}, options)
-let networkData: vis.Data = {
-  nodes: new vis.DataSet(),
-  edges: new vis.DataSet(),
-}
+let networkData: vis.Data = {}
 
 const redraw = () => {
   const input_text = (document.getElementById('input_text') as HTMLInputElement)
@@ -24,27 +21,19 @@ const redraw = () => {
     edges: new vis.DataSet(json.edges),
   }
   network.setData(networkData)
-  network.moveTo({ scale: 2.0 })
 }
 
 const main = () => {
-  console.log('vis_sufixtree.ts main')
   const input_text = document.getElementById('input_text') as HTMLElement
   input_text.addEventListener('input', redraw)
   input_text.addEventListener('propertychange', redraw)
-  networkData = {
-    nodes: new vis.DataSet(),
-    edges: new vis.DataSet(),
-  }
   network.on('hoverEdge', e => {
     console.log('hoverEdge', e)
     networkData.edges.update({ id: e.edge, font: { size: 34 } })
-    // network.body.data.edges.update({id: e.edge, font: {size : 34}})
   })
   network.on('blurEdge', e => {
     console.log('blurEdge', e)
     networkData.edges.update({ id: e.edge, font: { size: 14 } })
-    // network.body.data.edges.update({id: e.edge, font: {size : 14}})
   })
   redraw()
 }
